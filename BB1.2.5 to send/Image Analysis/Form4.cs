39df3442e92db7a437b26c7c9a9c209e90295f2a
madlajos <madlajos96@gmail.com>
2016-07-13 22:48:57 +0200

    BB125

---
+++ BB1.2.5 to send/Image Analysis/Form4.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Jai_FactoryDotNET;
+using System.Threading;
+using System.Runtime.InteropServices;
+using System.IO;
+
+
+namespace Image_Analysis
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
